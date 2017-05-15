a4a2f0913f648a1ebcf3a4ec0eba96267fdee7b9
Andrea Yee <andrea_jy@hotmail.com>
2017-05-15 14:31:57 +1000

    no message

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
     $("#quizPage2").hide();
+    $("#quizMe-page").hide();
 
-    $("#takeQuizBtn").click(function() {
+    $("#DNA-btn-2").click(function() {
 	    $("#quizPage1").hide();
 	    $("#quizPage2").show();
     });
 
+    $("#start_btn").click(function() {
+	    $("#quizPage2").hide();
+	    $("#quizMe-page").show();
+    });
+
+    $("#dropdownTest").hover(
+					function(){
+						$("#myDropdown").show();
+					}, function(){
+						$("#myDropdown").show();
+				});
+	$("#myDropdown").hover(
+					function(){
+						$("#myDropdown").show();
+					}, function(){
+						$("#myDropdown").hide();
+				});
 });
